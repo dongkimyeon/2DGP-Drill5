c04690699949679c97e736e8f3852e48c0f74489
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-09-25 16:06:31 +0900

    캐릭터 기본 틀 준비

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,6 +1,22 @@
 from pico2d import *
 from enum import Enum
 
+class Direction(Enum):
+    RIGHT = 0
+    LEFT = 1
+    UP = 2
+    DOWN = 3
+    NONE = 4
+
+class Character:
+    def __init__(self):
+        self.x, self.y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+        self.dir = Direction.NONE
+        self.speed = 10
+        self.animation_image = load_image('animation_sheet.png')
+        self.frame = 0
+
+
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
@@ -8,9 +24,12 @@ tuk_ground = load_image('TUK_GROUND.png')
 
 
 
+
 while True:
-   tuk_ground.draw_now(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-   pass
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    update_canvas()
+    pass
 
 
 close_canvas()
